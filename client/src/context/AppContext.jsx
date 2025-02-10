3a967ab7e78441b594daf53262ff70cbd0d199da
Kavya Jaimini <kavyajaimini39@gmail.com>
2025-02-10 10:49:56 +0530

    Landing Page Completed
    Landing page has been created having sidebar and job cards

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,35 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jobsData } from "../assets/assets";
 
-export const AppContext=createContext()
+export const AppContext = createContext()
 
 export const AppContextProvider=(props)=>{
+
+    const [searchFilter,setSearchFilter] = useState({
+        title:'',
+        location:''
+    })
+
+    const [isSearched,setIsSearched]=useState(false)
+
+    const [jobs, setJobs] = useState([])
+
+    // Function to fetch jobs data
+    const fetchJobs = async () => {
+        setJobs(jobsData)
+    }
+    
+    useEffect(()=>{
+        fetchJobs()
+    },[])
+
+
+
     const value={
+        setSearchFilter,searchFilter,
+        isSearched,setIsSearched,
+        jobs, setJobs
+
 
     }
 
