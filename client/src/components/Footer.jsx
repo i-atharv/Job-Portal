0a6997b9840b353764ed7a664ac35b7fc691f328
Atharva <iamatharvjaiswal@gmail.com>
2025-04-16 23:10:31 +0530

    Update v1

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import {assets} from '../assets/assets' ; 
+import React from "react";
+import { assets } from "../assets/assets";
 
 function Footer() {
   return (
-    <div className='container px-4 2xl:px-20 mx-auto flex  items-center justify-between gap-4 py-3 mt-20'> 
-      <img width={160} src ={assets.logo} alt="" />
-      <p className='flex-1  border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @GreatStack.dev | All right reserved. </p>
-      <div className='flex gap-2.5'>
-        <img src='assets.facebook_icon' alt=''/>
-        <img src='assets.twitter_icon' alt=''/>
-        <img src='assets.instagram_icon' alt=''/>
+    <div className="container px-4 2xl:px-20 mx-auto flex  items-center justify-between gap-4 py-3 mt-20">
+      <img width={160} src={assets.logo} alt="" />
+      <p className="flex-1  border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">
+        Copyright InsiderJobs | All right reserved.{" "}
+      </p>
+      <div className="flex gap-2.5">
+        <img src="assets.facebook_icon" alt="" />
+        <img src="assets.twitter_icon" alt="" />
+        <img src="assets.instagram_icon" alt="" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,42 +1,41 @@
 import { createContext, useEffect, useState } from "react";
 import { jobsData } from "../assets/assets";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-export const AppContextProvider=(props)=>{
+export const AppContextProvider = (props) => {
+  const [searchFilter, setSearchFilter] = useState({
+    title: "",
+    location: "",
+  });
 
-    const [searchFilter,setSearchFilter] = useState({
-        title:'',
-        location:''
-    })
+  const [isSearched, setIsSearched] = useState(false);
 
-    const [isSearched,setIsSearched]=useState(false)
+  const [jobs, setJobs] = useState([]);
 
-    const [jobs, setJobs] = useState([])
+  const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
 
-    const [showRecruiterLogin,setShowRecruiterLogin] = useState(false)
+  // Function to fetch jobs data
+  const fetchJobs = async () => {
+    setJobs(jobsData);
+  };
 
-    // Function to fetch jobs data
-    const fetchJobs = async () => {
-        setJobs(jobsData)
-    }
-    
-    useEffect(()=>{
-        fetchJobs()
-    },[])
+  useEffect(() => {
+    fetchJobs();
+  }, []);
 
+  const value = {
+    setSearchFilter,
+    searchFilter,
+    isSearched,
+    setIsSearched,
+    jobs,
+    setJobs,
+    showRecruiterLogin,
+    setShowRecruiterLogin,
+  };
 
-
-    const value={
-        setSearchFilter,searchFilter,
-        isSearched,setIsSearched,
-        jobs, setJobs,
-        showRecruiterLogin, setShowRecruiterLogin
-    }
-
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
