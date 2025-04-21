64c48b3aba10f3d2c7f51d8b561976c762eba5c4
Atharva <iamatharvjaiswal@gmail.com>
2025-04-21 20:30:04 +0530

    update v3

--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
-import './config/instrument.js'
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/db.js'
-import * as Sentry from '@sentry/node'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import './config/instrument.js';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js';
 
+
+//Initialize Express
 const app = express()
 
+//connect to database
 await connectDB()
 
+//Middlewares
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('API Working'))
-app.get('/debug-sentry', () => { throw new Error("My first Sentry error!") })
-app.post('/webhooks', clerkWebhooks)
+//Routes
+app.get('/',(req,res)=> res.send("API Working"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
 
+  app.post('/webhooks',clerkWebhooks)
+  
+  
+
+//Port
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT,()=>{
+    console.log('Server is running on ${PORT}');
+})
